bbd339b05617be36c8807fad732178ea14618412
Burn <burnb83@gmail.com>
2013-11-30 19:50:51 +0400

    fix phone view

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include_once "module.php";
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Тестовое задание</title>
     <script src="js/jquery.min.js"></script>
     <script src="js/jquery-ui.js"></script>
